d386c076d6704af802ba8a7d4a7aeff2f595fc03
dhanuprys <dhanuprys@gmail.com>
2025-09-22 07:21:36 +0800

    fixing

--- resources/views/filament/pages/income-report.blade.php
+++ resources/views/filament/pages/income-report.blade.php
@@ -42,19 +42,5 @@
                 </div>
             </div>
         </div>
-        
-        <!-- Header Widgets -->
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-            @foreach($this->getHeaderWidgets() as $widget)
-                @livewire($widget)
-            @endforeach
-        </div>
-        
-        <!-- Footer Widgets -->
-        <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
-            @foreach($this->getFooterWidgets() as $widget)
-                @livewire($widget)
-            @endforeach
-        </div>
     </div>
 </x-filament-panels::page>
